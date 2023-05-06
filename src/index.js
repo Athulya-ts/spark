d63917e89eacbc0bcf1acabd63de698a28a1fbe2
Athulya-ts <113665715+Athulya-ts@users.noreply.github.com>
2023-05-06 14:55:24 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import RouterPage from './RouterPage';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <RouterPage/>
+);
+
+
